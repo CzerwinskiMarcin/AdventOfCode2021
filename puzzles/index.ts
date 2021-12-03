942d03d55aa569ca8655364dec9be12f20886cc0
Marcin Czerwinski <marbroczerwinski@gmail.com>
2021-12-03 23:54:48 +0100

    add yargs for parametrization

---
+++ puzzles/index.ts
@@ -0,0 +1,31 @@
+const yargs = require('yargs/yargs');
+const {hideBin} = require('yargs/helpers');
+const path = require('path');
+
+const argv = yargs(hideBin(process.argv))
+    .option('d', {
+        alias: 'day',
+        demandOption: true,
+        describe: 'The puzzle from the day',
+        type: 'number'
+    })
+    .option('s', {
+        alias: 'source',
+        default: 'example',
+        describe: 'Source of the data for the puzzle',
+        choices: ['example', 'puzzle']
+    })
+    .argv;
+
+
+const {day, source} = argv;
+const formattedDay = `${day}`.padStart(2, '0');
+const targetPath = path.join(__dirname, formattedDay);
+
+import(targetPath)
+    .then(({default: fn}: {default: Function}) => {
+        fn(path.join('data', `${source}.txt`));
+    })
+    .catch(err => {
+        console.error(err);
+    });
