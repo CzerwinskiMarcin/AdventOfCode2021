d7978f6361b5838caa32f5b885aacf8fdb9ec319
Marcin Czerwinski <marbroczerwinski@gmail.com>
2021-12-04 22:54:19 +0100

    change third day solution to keep convenction in returnign values

--- puzzles/03/index.ts
+++ puzzles/03/index.ts
@@ -1,11 +1,16 @@
 import { FileUtils } from "../shared/utils";
 import { DiagnosticReportInterpreter } from "./models/diagnostic-report-interpreter.model";
 
-export default function main(sourcePath: string): void {
+export default function main(sourcePath: string): {alphaResult: number, betaResult: number} {
     const diagnosticInterpreter = new DiagnosticReportInterpreter();
     const sourceData: Array<string> = FileUtils.readFileToArray(sourcePath);
 
     const diagnosticResult = diagnosticInterpreter.interpretReport(sourceData);
 
     console.log(diagnosticResult);
+
+    return {
+        alphaResult: diagnosticResult.consumptionData.powerConsumption,
+        betaResult: diagnosticResult.lifeSupportData.lifeSupportRating
+    }
 }
